7a38506798ee6496f098a4d198462ad2e8b9a8aa
Bhisham Balhara <115534824+bhisu786@users.noreply.github.com>
2024-09-18 12:43:10 +0530

    Update thread-pool.js

--- thread-pool.js
+++ thread-pool.js
@@ -3,6 +3,22 @@ const crypto = require("crypto");
 
 process.env.UV_THREADPOOL_SIZE=8;
 
+
+// EASY WAY TO SIMPLY UNDERSTAND THE STRUCTURE OF THE CRYPTO.PBKDF:-
+// const crypto = require('crypto');
+
+// const password = 'secret';
+// const salt = 'salt';
+// const iterations = 1000;
+// const keylen = 64;
+// const digest = 'sha512'; // Correct digest algorithm
+
+// crypto.pbkdf2(password, salt, iterations, keylen, digest, (err, derivedKey) => {
+//   if (err) throw err;
+//   console.log(derivedKey.toString('hex'));
+// });
+
+
 crypto.pbkdf2("password","salt",5000000,50,"sha512",(err,key)=> {
   console.log("1-CryptoPBKDF2 DONE");
 });
