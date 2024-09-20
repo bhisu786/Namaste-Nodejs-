b9ca8480898ddb8d7914bd1244a922084f0cb117
Bhisham Balhara <115534824+bhisu786@users.noreply.github.com>
2024-09-20 11:42:14 +0530

    .

---
+++ database.js
@@ -0,0 +1,54 @@
+// PassWord For the Mongodb :- "hEXaRJbwj3afl6oN"
+
+const {MongoClient} = require("mongodb");
+
+
+const url = "mongodb+srv://balharabhisham:hEXaRJbwj3afl6oN@cluster0.7vefpjn.mongodb.net/NamasteNodeJS";
+
+const client  = new MongoClient(url);
+
+
+const dbName ="HelloWorld";
+ 
+
+ async function main() {
+    await client.connect();
+ console.log('Connected successfully to Server');
+ const db =  client.db(dbName);
+ const collection = db.collection('Users');
+
+ // the following code example can be paste here
+ const Data  ={
+    first_name:"Bhisham ",
+    last_name:"Balhara",
+    city:"Panipat",
+    phoneNumber:"9876543210",
+ };
+   
+  const insertResult = await collection.insertOne(Data);
+  console.log("Inserted Documents =>",insertResult);
+
+  // Readd the document
+  const findResult =await collection.find({}).toArray();
+  console.log("Found Documents =>",findResult);
+
+  const countResult =await collection.countDocuments({});
+   console.log("Count of Documents in the user Collection =>",countResult);
+
+   // find all the documents with a filter of First_Name :Bhisham
+    
+   // const result =await collection.find({first_name:"Bhishu"}).count();
+   // console.log("result=> ",result);
+     
+   const result = await collection.countDocuments({first_name: "Bhishu"});
+console.log("result =>", result); 
+   return "done";
+
+ }
+
+ main()
+ .then(console.log)
+ .catch(console.error)
+ .finally(()=>client.close());
+
+ 
---
+++ server.js
@@ -0,0 +1,7 @@
+const http =   require("http");
+
+const server = http.createServer(function (req,res) {
+    res.end("Hello world");
+});
+
+server.listen(1000 );
